6d770d39cb0ae0b718a327c65d9c784637855dde
Fatima MAAYANE <fatimamaayane@gmail.com>
2024-03-27 12:11:55 +0100

    add pages

--- react-blog-website/src/main.jsx
+++ react-blog-website/src/main.jsx
@@ -7,11 +7,39 @@ import {
   Route,
   RouterProvider,
 }from "react-router-dom"
+import Home from './pages/Home.jsx'
+import Blogs from './pages/Blogs.jsx'
+import About from'./pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Services from './pages/Services.jsx'
 
 const router = createBrowserRouter([
   {
     path:"/",
-    element: <div>Hello World</div>
+    element: <App/>,
+    children:[
+      {
+        path:"/",
+        element: <Home/>
+      },
+      {
+        path:"/blogs",
+        element:<Blogs/>
+      }, 
+      {
+        path:"/about",
+        element:<About/>
+      },
+      {
+        path:"/contact",
+        element:<Contact/>
+      }, 
+      {
+        path:"/services",
+        element:<Services/>
+      }
+    ]
+    
   }
 ])
 
